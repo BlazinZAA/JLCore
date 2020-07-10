b6160dbcf8ece5cb2880b39cca973970c9ba42c9
BlazinZAA <BlazinZAA082@gmail.com>
2020-07-10 16:56:48 +0400

    There we go

--- src/net/viedantmc/JLCore/Main.java
+++ src/net/viedantmc/JLCore/Main.java
@@ -1,5 +1,25 @@
 package net.viedantmc.JLCore;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.viedantmc.JLCore.events.Fireworks;
+import net.viedantmc.JLCore.files.DataManager;
+
+public class Main extends JavaPlugin {
+	private DataManager data;
+	private Main plugin;
+//what the fuck
+	@Override
+	public void onEnable(){
+		 plugin = this;
+		 this.data = new DataManager(this);
+	     getServer().getPluginManager().registerEvents(new Fireworks(), this);
+
+	}
+	
+	@Override
+	public void onDisable(){}
+	
+	
 
 }
