ac8322d8e878f729460359773405eb99b7eb4bf9
BlazinZAA <BlazinZAA082@gmail.com>
2020-07-10 16:48:20 +0400

    Trying to fix stuff that doesnt wanna be fixed

--- src/net/viedantmc/JLCore/commands/fireworkcommand.java
+++ src/net/viedantmc/JLCore/commands/fireworkcommand.java
@@ -1,5 +1,32 @@
 package net.viedantmc.JLCore.commands;
 
-public class fireworkcommand {
+import net.viedantmc.JLCore.Main;
+import net.viedantmc.JLCore.files.DataManager;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class fireworkcommand implements CommandExecutor{
+	public DataManager data;
+	private Main plugin;
+  
+    public fireworkcommand(Main plugin) {
+    	this.plugin = plugin;
+    }
+    @Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	
+		if(label.equalsIgnoreCase("JLCoreFireworks")) 
+			if(sender instanceof Player)
+				if(args.length != 0)
+					if(sender.hasPermission("JLCore.Fireworks")) {
+						Player player = (Player) sender;
+						this.data.getConfig().set("fireworklocation" + args[0], player.getLocation());
+					}
+		return false;
+	}
+	
 
 }
--- src/net/viedantmc/JLCore/files/DataManager.java
+++ src/net/viedantmc/JLCore/files/DataManager.java
@@ -1,5 +1,67 @@
 package net.viedantmc.JLCore.files;
 
-public class DataManager {
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Entity;
+
+import net.viedantmc.JLCore.Main;
+
+public class DataManager {
+	private Main plugin;
+	private FileConfiguration dataConfig = null;
+	private File configFile = null;
+	
+	public DataManager(Main plugin) {
+		this.plugin = plugin;
+		saveDefaultConfig();
+	}
+	public void reloadConfig() {
+		if(this.configFile == null)
+			this.configFile = new File(this.plugin.getDataFolder(), "config.yml");
+		
+		this.dataConfig = YamlConfiguration.loadConfiguration(this.configFile);
+		
+		InputStream defaultStream = this.plugin.getResource("config.yml");
+		
+		if (defaultStream != null) {
+			YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
+			this.dataConfig.setDefaults(defaultConfig);
+		}
+		
+		}
+		
+		
+
+	public FileConfiguration getConfig() {
+		if (dataConfig != null) 
+			reloadConfig();
+		return this.dataConfig;
+	
+	}
+	public void saveConfig() {
+		if(this.configFile == null || this.dataConfig == null) {
+			return;
+		}
+		try {
+		this.getConfig().save(this.configFile); 
+		} catch (IOException e) {
+			plugin.getLogger().log(Level.SEVERE, "Could not save config" + this.configFile, e);
+		}
+	}
+	
+	public void saveDefaultConfig() {
+		if (this.configFile == null) {
+			this.configFile = new File(this.plugin.getDataFolder(), "config.yml");
+			
+		}
+		if (!this.configFile.exists()) 
+			this.plugin.saveResource("config.yml", false);
+	}
 }
+	
